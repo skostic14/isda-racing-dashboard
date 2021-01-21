95dcdc396abca5293512b6778e63fb9bcee9058f
Slobodan Kostic <slobodan.kostic14@gmail.com>
2021-01-21 20:01:02 +0100

    Fix Signup URL

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
-      <Switch>
-        <Route path="/index"><SignUp/></Route>
-        <Route path="/results"><RaceResults/></Route>
-      </Switch>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/signup" component={SignUp}></Route>
+          <Route path="/results" component={RaceResults}></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
