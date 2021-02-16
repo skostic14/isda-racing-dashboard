fbdb57059d48a69813c459d832f21b319d18e53b
Slobodan Kostic <slobodan.kostic14@gmail.com>
2021-02-16 10:56:30 +0100

    Added standings page and display

---
+++ src/RaceResults/DriverStanding.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import './RaceResults.css';
+
+const renderDriverStanding = (props) => {
+
+    return (
+        <tr>
+            <td>{props.position}</td>
+            <td>{props.driverName}</td>
+            <td>{props.team}</td>
+            {
+                props.results.map((position) => {
+                    let styleModifier = null;
+                    if (position === 1) {
+                        styleModifier = 'firstPosition';
+                    }
+                    else if (position === 2) {
+                        styleModifier = 'secondPosition';
+                    }
+                    else if (position === 3) {
+                        styleModifier = 'thirdPosition';
+                    }
+                    else if (position === 'DSQ') {
+                        styleModifier = 'DSQ';
+                    }
+                    return (<td className={styleModifier}>{position}</td>)
+                })
+            }
+            <td>{props.points}</td>
+        </tr>
+    )
+}
+
+export default renderDriverStanding;
