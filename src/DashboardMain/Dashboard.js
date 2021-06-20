421fc3740209abd0f547145c71196a6de0bb5af1
Slobodan Kostic <slobodan.kostic14@gmail.com>
2021-06-20 21:20:22 +0200

    Remove Kyalami and Pcup signups

--- src/DashboardMain/Dashboard.js
+++ src/DashboardMain/Dashboard.js
@@ -42,38 +42,6 @@ export default function Dashboard() {
         return currentUser
     }, []);
 
-    const kyalamiSignUp = (
-        <div className="container w-75">
-            <h4>9h of Kyalami</h4>
-            <h4>May 22 - 15:00 CEST</h4>
-            <h4 className="mb-4">Team registration</h4>
-            <Endurance_SignUp season="ACC_OneOff_Kyalami9h" maxDrivers="4"/>
-        </div>)
-    
-    const kyalamiUpdate = (
-        <div className="container w-75">
-            <h4>9h of Kyalami</h4>
-            <h4>May 22 - 15:00 CEST</h4>
-            <h4 className="mb-4">Team update</h4>
-            <Endurance_TeamUpdate season="ACC_OneOff_Kyalami9h" maxDrivers="4"/>
-        </div>
-    )
-
-    const pcupSignUp = (
-        <div className="container w-75">
-            <h4>2021 ISDA Porsche Cup Challenge - Spring Season</h4>
-            <h4 className="mb-4">Team registration</h4>
-            <Endurance_SignUp season="ACC_Pcup_S1" maxDrivers="2"/>
-        </div>)
-    
-    const pcupUpdate = (
-        <div className="container w-75">
-            <h4>2021 ISDA Porsche Cup Challenge - Spring Season</h4>
-            <h4 className="mb-4">Team update</h4>
-            <Endurance_TeamUpdate season="ACC_Pcup_S1" maxDrivers="2"/>
-        </div>
-    )
-
     return (
         <div className="h-100 bg-light">
             <Navbar bg="dark" variant="dark">
@@ -87,7 +55,7 @@ export default function Dashboard() {
                     </NavDropdown>
                     <Nav.Link onClick={() => setComponent('Calendar')}>Calendar</Nav.Link>
                     <Nav.Link onClick={() => setComponent('SignUpList')}>Entry Lists</Nav.Link>
-                    <Nav.Link onClick={() => setComponent('IncidentReportForm')}>Incident report (NEW)</Nav.Link>
+                    <Nav.Link onClick={() => setComponent('IncidentReportForm')}>Incident report</Nav.Link>
                 </Nav>
                 <Nav className="float-right">
                     <Nav.Link className="mr-auto">{currentDriver}</Nav.Link>
@@ -100,12 +68,6 @@ export default function Dashboard() {
                 <Navbar className="col-md-2 d-none d-md-block bg-light sidebar">
                     <Button variant="light outline-primary" className="mt-2 w-100"><a href='https://www.isdaracing.com'>ISDA Homepage</a></Button>
                     <Button variant="light outline-primary" className="mt-2 w-100"><a href='https://discord.gg/KZqV4Hfb9A'>ISDA Discord</a></Button>
-                    <hr/>
-                    <Button variant="light outline-primary" className="mt-2 w-100" onClick={() => setComponent('9hKyalamiSignUp')}><a href='#'>9h of Kyalami - Team Registration</a></Button>
-                    <Button variant="light outline-primary" className="mt-2 w-100" onClick={() => setComponent('9hKyalamiTeamUpdate')}><a href='#'>9h of Kyalami - Team Update</a></Button>
-                    <hr/>
-                    <Button variant="light outline-primary" className="mt-2 w-100" onClick={() => setComponent('PcupSignUp')}><a href='#'>Porsche Cup Challenge - Registration</a></Button>
-                    <Button variant="light outline-primary" className="mt-2 w-100" onClick={() => setComponent('PcupTeamUpdate')}><a href='#'>Porsche Cup Challenge - Team Update</a></Button>
                 </Navbar>
                 <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4 w-100 mt-4">
                     <div className="w-100">
@@ -115,19 +77,12 @@ export default function Dashboard() {
                             <h1 className="mt-5">Welcome to International Sim Drivers Association!</h1>
                             <h3 className="mt-5">Please note that the dashboard is in development and that features will be gradually added.</h3>
                             <h3>For more information about dashboard updates, keep an eye on our Discord!</h3>
-
-                            <Link onClick={() => setComponent('9hKyalamiSignUp')}><h1 className="mt-4 link">Sign up for 9h of Kyalami - May 22</h1></Link>
-                            <Link onClick={() => setComponent('PcupSignUp')}><h1 className="mt-4 link">Sign up for 2021 ISDA Porsche Cup Challenge - Spring Season</h1></Link>
                         </div>}
                         {component==='RaceResults' && <RaceResults/>}
                         {component==='Calendar' && <Calendar/>}
                         {component==='SeasonStandings' && <SeasonStandings/>}
                         {component==='GT3Signup' && <GT3Signup/>}
                         {component==='SignUpList' && <SignUpList/>}
-                        {component==='9hKyalamiSignUp' && kyalamiSignUp}
-                        {component==='9hKyalamiTeamUpdate' && kyalamiUpdate}
-                        {component==='PcupSignUp' && pcupSignUp}
-                        {component==='PcupTeamUpdate' && pcupUpdate}
                         {component==='IncidentReportForm' && <IncidentReportForm/>}
                     </div> 
                 </main>
