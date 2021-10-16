94d24468e2c341c41b33c00234034e2ed229b4de
Slobodan Kostic <slobodan.kostic14@gmail.com>
2021-10-16 12:22:27 +0200

    3h of Zolder signups

--- src/DashboardMain/Dashboard.js
+++ src/DashboardMain/Dashboard.js
@@ -46,16 +46,16 @@ export default function Dashboard() {
 
     const zolderSignUp = (
         <div className="container w-75">
-            <h4>8h of Zolder</h4>
-            <h4>October 23 - 15:00 CEST</h4>
+            <h4>3h of Zolder</h4>
+            <h4>October 23 - 17:00 CEST</h4>
             <h4 className="mb-4">Team registration</h4>
-            <Endurance_SignUp season="ACC_OneOff_8hZolder_Oct23" maxDrivers="4"/>
+            <Endurance_SignUp season="ACC_OneOff_8hZolder_Oct23" maxDrivers="2"/>
         </div>)
 
     const zolderUpdate = (
         <div className="container w-75">
-            <h4>8h of Zolder</h4>
-            <h4>October 23 - 15:00 CEST</h4>
+            <h4>3h of Zolder</h4>
+            <h4>October 23 - 17:00 CEST</h4>
             <h4 className="mb-4">Team update</h4>
             <Endurance_TeamUpdate season="ACC_OneOff_8hZolder_Oct23" maxDrivers="4"/>
         </div>
@@ -101,8 +101,8 @@ export default function Dashboard() {
                     <Button variant="light outline-primary" className="mt-2 w-100" onClick={() => setComponent('GT3Signup')}><a href='#'>Register for 2021 ISDA GT3 World Challenge - Autumn Season</a></Button>
                     <Button variant="light outline-primary" className="mt-2 w-100"><a href='https://forms.gle/4c3yFp5SehgVerzZ6'>2021 ISDA GT3 World Challenge - Autumn Season - Livery Upload</a></Button>
                     <hr/>
-                    <Button variant="light outline-primary" className="mt-2 w-100" onClick={() => setComponent('zolderSignUp')}><a href='#'>8h of Zolder - Team Registration</a></Button>
-                    <Button variant="light outline-primary" className="mt-2 w-100" onClick={() => setComponent('zolderUpdate')}><a href='#'>8h of Zolder - Team Update</a></Button>
+                    <Button variant="light outline-primary" className="mt-2 w-100" onClick={() => setComponent('zolderSignUp')}><a href='#'>3h of Zolder - Team Registration</a></Button>
+                    <Button variant="light outline-primary" className="mt-2 w-100" onClick={() => setComponent('zolderUpdate')}><a href='#'>3h of Zolder - Team Update</a></Button>
                 </Navbar>
                 <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4 w-100 mt-4">
                     <div className="w-100">
@@ -118,8 +118,8 @@ export default function Dashboard() {
                                 </div>
                                 <div className="col-md-6">
                                     <Link onClick={() => setComponent('zolderSignUp')}>
-                                    <img className="img-fluid" alt="ISDA 8h of Zolder" src="https://www.isdaracing.com/wp-content/uploads/2021/09/zolder1.4.png"></img>
-                                    <h3 className="mt-2 link">Register for 8h of Zolder - October 23rd</h3>
+                                    <img className="img-fluid" alt="ISDA 3h of Zolder" src="https://www.isdaracing.com/wp-content/uploads/2021/09/zolder1.4.png"></img>
+                                    <h3 className="mt-2 link">Register for 3h of Zolder - October 23rd</h3>
                                     </Link>
                                 </div>
                             </div>
