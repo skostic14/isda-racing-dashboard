b321f33846ac7e28164081bc7913c00625994386
Slobodan Kostic <slobodan.kostic14@gmail.com>
2022-02-24 07:24:36 +0100

    LamboST Signup

--- src/SignUp/GT3_Signup.js
+++ src/SignUp/GT3_Signup.js
@@ -15,7 +15,7 @@ export default function GT3Signup() {
     const [loading, setLoading] = useState(false)
     const [responseCode, setResponseCode] = useState()
     const history = useHistory()
-    const season='ACC_GT3_S3'
+    const season='ACC_SuperTrofeo_S1'
 
     useEffect(() => {
         if (currentDriver === undefined || !currentDriver) {
@@ -86,7 +86,7 @@ export default function GT3Signup() {
 
     return (
         <div align="center">
-            <h3 className="text-center mt-4 mb-4">Sign up for 2022 ISDA GT3 World Challenge - Winter Season</h3>
+            <h3 className="text-center mt-4 mb-4">Sign up for ACC - 2022 ISDA Lamborghini SuperTrofeo - Winter Season</h3>
             <Form className="mb-4 w-50" onSubmit={submitCarSelection}>
                 <Form.Group id="car">
                     <Form.Label>Car Type</Form.Label>
